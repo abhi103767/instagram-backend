b3b7dfd73fa84c0785e6768c3df984f8a2f567c5
Avinash <avinash1234sharma1@gmail.com>
2022-06-10 18:15:10 +0530

    connecting to feed url

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const authRouter = require('./routes/auth');
 const userRouter = require('./routes/user');
 const connect = require('./storage/db')
-
+const cors = require('cors');
+app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use('/auth', authRouter);
